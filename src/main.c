4b5cdf238f95f28efd0500f70ebb47e95f9943fd
Raúl Alonso Pardo <raul@Rauls-MacBook-Air.local>
2024-02-06 13:06:00 +0100

    cd fix in main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: raalonso <raalonso@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 19:02:41 by raalonso          #+#    #+#             */
-/*   Updated: 2024/02/06 12:25:45 by raalonso         ###   ########.fr       */
+/*   Updated: 2024/02/06 13:05:10 by raalonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,15 @@ int	main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	do
 	{
+		free(str);
 		str = readline(">> ");
 		if (ft_strcmp(str, "pwd") == 0)
 			msh_pwd();
-		if (ft_strnstr(str, "cd", ft_strlen(str)) != NULL)
-			msh_cd(ft_split(str, ' ')[1]);
+		else if (ft_strnstr(str, "cd", ft_strlen(str)) != NULL)
+			msh_cd(ft_substr(str, ft_strlen(ft_split(str, ' ')[0]) + 1,
+					ft_strlen(str) - (ft_strlen(ft_split(str, ' ')[0]) + 1)));
+		else if (ft_strcmp(str, "ls") == 0)
+			system("ls");
 	} while (ft_strcmp(str, "exit") != 0);
 	free(str);
 	return (EXIT_SUCCESS);
