69e5bebe76f70adacb53e81c585dcf6199afb91c
Miguel Guardia <mike.guardia0309@gmail.com>
2024-02-13 19:17:44 +0100

    Makefile && minishell.h updated

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 19:01:57 by raalonso          #+#    #+#             */
-/*   Updated: 2024/02/02 17:54:00 by mguardia         ###   ########.fr       */
+/*   Updated: 2024/02/13 18:02:40 by mguardia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,67 @@
 #include <signal.h>				// signal, sigaction, kill
 #include <string.h>				// strerror
 #include <errno.h>				// para manejar errores
+
+/*
+*	Typedefs
+*/
+typedef struct s_shell		t_shell;
+typedef struct s_env		t_env;
+typedef struct s_env_list	t_env_list;
+typedef struct s_line_p		t_line_p;
+
+/*
+*	Structs
+*/
+// env nodes
+struct s_env
+{
+	char	*key;		// siempre existe una key, lo que puede no existir es un value "hola=" es permitido.
+	char	*value;
+};
+
+struct	s_env_list
+{
+	t_env		*content;
+	t_env_list	*next;
+};
+
+// line parsed nodes
+struct s_line_p
+{
+	char		*content;
+	t_line_p	*next;
+};
+
+// general struct
+struct	s_shell
+{
+	char		*line_read;
+	t_env_list	*envi;
+	t_line_p	*string_list;
+
+};
+
+/*
+*	ENV
+*/
+int		create_env_list(t_shell *shell, char **env);
+t_env	*set_env_content(char *env_str);
+int		create_new_env(t_env_list **envi, t_env *node_content);
+
+/*
+*	BUILTINS
+*/
+int		ft_env(t_env_list **envi, t_line_p *args);
+int		ft_export(t_env_list **envi, t_line_p *args);
+int		print_export(t_env_list **envi);
+int		ft_unset(t_env_list **envi, t_line_p *args);
+
+/*
+*	UTILS
+*/
+void	free_env(void *content);
+bool	already_exists(t_env_list **envi, char *key);
+
 
 #endif
