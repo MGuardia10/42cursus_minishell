63213c5181d9f4a02560939bd288ca2e4d894bbc
Raúl Alonso Pardo <raul@Rauls-MacBook-Air.local>
2024-03-09 16:33:00 +0100

    fix

--- src/parse/init.c
+++ src/parse/init.c
@@ -6,7 +6,7 @@
 /*   By: raalonso <raalonso@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 21:43:59 by raalonso          #+#    #+#             */
-/*   Updated: 2024/02/29 10:09:53 by raalonso         ###   ########.fr       */
+/*   Updated: 2024/03/06 17:49:01 by raalonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,6 +88,83 @@ int	num_of_tokens(char *line)
 	return (count);
 }
 
+int	is_special_char(char c)
+{
+	if (c == ' ' || c == '|' || c == '>' || c == '"' || c == '\'')
+		return (0);
+	return (1);
+}
+
+int	quotes_token(char *line, char **cmds, int *i, int *j)
+{
+	int last;
+
+	*i += 1;
+	last = *i;
+	if (line[*i] == '"')
+	{
+		while (line[*i] && line[*i] != '"')
+			*i += 1;
+	}
+	else if (line[*i] == '\'')
+	{
+		while (line[*i] && line[*i] != '\'')
+			*i += 1;	
+	}
+	cmds[*j] = ft_substr(line, last, *i - last);
+	if (!cmds[*j])
+		return (1);
+	*i += 1;
+	*j += 1;
+	return (0);
+}
+
+int	handle_special_char(char *line, char **cmds, int *i, int *j)
+{
+	int	last;
+	
+	if (line[*i] == '"' || line[*i] == '\'')
+	{
+		if (quotes_token(line, cmds, i, j) == 1)
+			return (1);
+	}
+	else if (line[*i] == ' ')
+	{
+		while (line[*i] == ' ')
+			*i += 1;
+	}
+	else if (line[*i] == '|')
+	{
+		last = *i;
+		*i += 1;
+		cmds[*j] = ft_substr(line, last, *i - last);
+		if (!cmds[*j])
+			return (1);
+		*j += 1;
+	}
+	else if (line[*i] == '>')
+	{
+		last = *i;
+		if (line[*i + 1] == '>')
+		{
+			*i += 2;
+			cmds[*j] = ft_substr(line, last, *i - last);
+			if (!cmds[*j])
+				return (1);
+			*j += 1;
+		}
+		else
+		{
+			*i += 1;
+			cmds[*j] = ft_substr(line, last, *i - last);
+			if (!cmds[*j])
+				return (1);
+			j += 1;
+		}
+	}
+	return (0);
+}
+
 char	**get_tokens(char *line)
 {
 	int		i;
@@ -101,73 +178,20 @@ char	**get_tokens(char *line)
 	cmds = (char **)malloc(sizeof(char *) * (num_of_tokens(line) + 1));
 	while (line[i])
 	{
-		if (line[i] != ' ' && line[i] != '|' && line[i] != '>' && line[i] != '"' && line[i] != '\'')
+		if (is_special_char(line[i]) == 1)
 		{
 			last = i;
-			while (line[i] && line[i] != ' ' && line[i] != '|' && line[i] != '>' && line[i] != '"' && line[i] != '\'')
+			while (line[i] && is_special_char(line[i]) == 1)
 				i++;
 			cmds[j] = ft_substr(line, last, i - last);
 			if (!cmds)
 				return (NULL);
 			j++;
 		}
-		else if (line[i] == '"')
+		else
 		{
-			i++;
-			last = i;
-			while (line[i] && line[i] != '"')
-				i++;
-			cmds[j] = ft_substr(line, last, i - last);
-			if (!cmds)
+			if (handle_special_char(line, cmds, &i, &j) == 1)
 				return (NULL);
-			i++;
-			j++;
-		}
-		else if (line[i] == '\'')
-		{
-			i++;
-			last = i;
-			while (line[i] && line[i] != '\'')
-				i++;
-			cmds[j] = ft_substr(line, last, i - last);
-			if (!cmds)
-				return (NULL);
-			i++;
-			j++;
-		}
-		else if (line[i] == ' ')
-		{
-			while (line[i] == ' ')
-				i++;
-		}
-		else if (line[i] == '|')
-		{
-			last = i;
-			i++;
-			cmds[j] = ft_substr(line, last, i - last);
-			if (!cmds)
-				return (NULL);
-			j++;
-		}
-		else if (line[i] == '>')
-		{
-			last = i;
-			if (line[i + 1] == '>')
-			{
-				i += 2;
-				cmds[j] = ft_substr(line, last, i - last);
-				if (!cmds)
-					return (NULL);
-				j++;
-			}
-			else
-			{
-				i++;
-				cmds[j] = ft_substr(line, last, i - last);
-				if (!cmds)
-					return (NULL);
-				j++;
-			}
 		}
 	}
 	cmds[j] = NULL;
