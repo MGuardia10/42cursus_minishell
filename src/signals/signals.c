efe08a1f9ddd59545f2e8b29896a22601749b256
Miguel Guardia <mike.guardia0309@gmail.com>
2024-03-26 18:51:25 +0100

    Signals: signals finished with norm and implemented in executer

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -3,43 +3,102 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raalonso <raalonso@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 15:06:05 by mguardia          #+#    #+#             */
-/*   Updated: 2024/03/17 19:28:28 by raalonso         ###   ########.fr       */
+/*   Updated: 2024/03/26 16:20:23 by mguardia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-volatile sig_atomic_t g_sigint_received = 0;
+volatile sig_atomic_t	g_signal_status = 0;
 
-// quit signal
-void	ft_sigquit(int signal)
+/**
+ * The function `ft_sigint_heredoc` handles the SIGINT signal in a
+ * non-interactive mode by setting a specific signal status and performing
+ * certain actions.
+ * 
+ * @param signal an integer representing the signal that triggered the signal
+ * handler.
+ */
+void	ft_sigint_heredoc(int signal)
 {
 	(void)signal;
-	printf("Hola desde sigquit\n");
+	if (g_signal_status == NON_INTERACTIVE)
+	{
+		g_signal_status = SIGINT_HD;
+		ioctl(0, TIOCSTI, "\n");
+		rl_replace_line("", 0);
+		rl_on_new_line();
+	}
 }
-// interrup signal
+
+/**
+ * The function `ft_sigint_child` is a signal handler for the SIGINT signal in a
+ * child process.
+ * 
+ * @param signal an integer that represents the signal that triggered the signal
+ * handler. Not used during the funcion.
+ */
+void	ft_sigint_child(int signal)
+{
+	(void)signal;
+	g_signal_status = SIGINT_CHILD;
+	ft_putchar_fd('\n', STDERR_FILENO);
+}
+
+/**
+ * The function `ft_sigint` handles the SIGINT signal when shell is interactive.
+ * 
+ * @param signal an integer that represents the signal number that triggered the
+ * signal handler. Not used during the function.
+ */
 void	ft_sigint(int signal)
 {
 	(void)signal;
-	printf("\n");
-	rl_on_new_line();
-	//rl_replace_line("", 0);
-	rl_redisplay();
+	if (g_signal_status == INTERACTIVE || g_signal_status == SIGINT_FATHER)
+	{
+		ft_putchar_fd('\n', STDERR_FILENO);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
 }
 
-void	signal_setter(void)
+/**
+ * The function `ft_sigquit` handles the SIGQUIT signal by setting a global
+ * signal status and printing a message to the standard error.
+ * 
+ * @param signal an integer that represents the signal number that triggered
+ * the signal handler.
+ */
+void	ft_sigquit(int signal)
+{
+	(void)signal;
+	g_signal_status = SIGQUIT_CHILD;
+	ft_putstr_fd("Quit: 3\n", STDERR_FILENO);
+}
+
+/**
+ * The function `signal_handler` sets up signal handlers for SIGINT (CTRL + C)
+ * and SIGQUIT (CTRL + 4 or CTRL + \).
+ * 
+ * @param sigint_func The `sigint_func` parameter is a function pointer that
+ * specifies the signal handler function to be called when the `SIGINT` signal
+ * (interrupt signal) is received.
+ * @param sigquit_func The `sigquit_func` parameter is a function pointer that
+ * points to the function that will be called when the `SIGQUIT` signal is
+ * received.
+ */
+void	signal_handler(sig_t sigint_func, sig_t sigquit_func)
 {
 	struct sigaction	sa;
 
-	sa.sa_handler = ft_sigquit;
 	sa.sa_flags = SA_RESTART;
 	sigemptyset(&sa.sa_mask);
-	sigaction(SIGQUIT, &sa, NULL);
-	sa.sa_handler = ft_sigint;
-	sa.sa_flags = SA_RESTART;
-	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = sigint_func;
 	sigaction(SIGINT, &sa, NULL);
+	sa.sa_handler = sigquit_func;
+	sigaction(SIGQUIT, &sa, NULL);
 }
